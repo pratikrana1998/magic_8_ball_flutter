02c1ae5e2b130a29ef6fb7e4f58f84318778f3ce
pratikrana1998 <pratikrana1998@users.noreply.github.com>
2020-06-07 14:41:27 +0530

    added stateless widget for appbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Center(child: Text('Ask Me Anything')),
+        backgroundColor: Colors.blue[700],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset('images/ball1.png'),
+        )
+      ],
+    );
+  }
+}
